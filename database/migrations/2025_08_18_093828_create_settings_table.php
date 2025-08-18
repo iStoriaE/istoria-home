93e7cb6037db68577ff13e3cc5f4719b10ea691d
Mohammed Helaiwa <eng.helewa@gmail.com>
2025-08-18 14:13:25 +0300

    Convert settings value to json

--- database/migrations/2025_08_18_093828_create_settings_table.php
+++ database/migrations/2025_08_18_093828_create_settings_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->string('key');
-            $table->jsonb('value');
+            $table->json('value');
             $table->string('title');
             $table->string('description')->nullable();
             $table->timestamps();
